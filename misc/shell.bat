fdf98fe84ec86264faee91d305cb5df17ec7a230
droune2001 <nfauvet@gmail.com>
2017-05-27 09:57:05 +0200

    Mise a jour pour vc community 2017

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,4 +1,5 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set path=C:\home\dev\my_handmade_hero\misc;C:\home\4coder;%path%
 cd ..\code
